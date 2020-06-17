5662f8460f1b58a1b3c1fef2a74194fc8e38eb27
HarrisonBoland <Harrisonheke@gmail.com>
2020-06-17 22:53:57 +1200

    Get recipe info on click

--- Forkify/src/js/index.js
+++ Forkify/src/js/index.js
@@ -1,8 +1,7 @@
 import Search from './modals/Search';
+import Recipe from './modals/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
-// const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
-// const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
 
 /* Global state of the app
     - Search Object
@@ -12,6 +11,8 @@ import { elements, renderLoader, clearLoader } from './views/base';
 */
 const state = {};
 
+
+// SEARCH CONTROLLER
 const controlSearch = async () => {
     // 1. Get query from view
     const query = searchView.getInput(); // TODO
@@ -25,12 +26,16 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        // 4. Search for recipers
-        await state.search.getResults();
+        try {
+            // 4. Search for recipers
+            await state.search.getResults();
 
-        // 5. Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result)
+            // 5. Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result)
+        } catch (error) {
+            console.log(error);
+        }
     };
 };
 
@@ -47,3 +52,35 @@ elements.searchResPages.addEventListener('click', e => {
         searchView.renderResults(state.search.result, goToPage);
     };
 });
+
+
+// RECIPE CONTROLLER
+const controlRecipe = async () => {
+    // Get ID from URL
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if (id) {
+        // Prepare UI for changes
+
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+            // Get recipe data
+            await state.recipe.getRecipe();
+
+            // Calculate serving and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            //Render recipe
+            console.log(state.recipe);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
