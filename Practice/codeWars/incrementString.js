516ac3d2719cc9d38066e3625ed51b5623c77f0c
HarrisonBoland <Harrisonheke@gmail.com>
2020-09-18 19:57:40 +1200

    codewars incrementString

---
+++ Practice/codeWars/incrementString.js
@@ -0,0 +1,40 @@
+function incrementString (strng) {
+  // checks if string is empty
+  if (strng === '') {
+    return '1'
+  }
+
+  // regex for word and number
+  const regex = /[0-9]/g
+  const regex2 = /[^0-9]/g
+  
+  // puts number into an array and removes the word from the string
+  let numberArr = strng.match(regex)
+  let word = ''
+  if (regex2.test(strng)) {
+    word = strng.match(regex2).join('')
+  }
+  let number = 1
+
+  // checks the length of the number with zeros vs without + 1 and adds zeros accordingly
+  if (numberArr) {
+    numArrLength = numberArr.length
+    num = parseInt(numberArr.join('')) + 1
+    numLength = num.toString().split('').length
+    if (numLength > numArrLength) {
+      word += num
+    } else {
+      zero = '0'
+      word += zero.repeat(numArrLength - numLength) + num
+    }
+  } else {
+    return word + number
+  }
+  
+  return word
+}
+
+console.log(incrementString("1"))
+
+// /(^9+$)/ 
+// regex3.test(numberArr.join(''))
